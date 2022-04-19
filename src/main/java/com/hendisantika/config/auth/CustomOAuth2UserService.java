d563b804009ad2b44ed25522dd41f9751fd3a891
Hendi Santika <hendisantika@gmail.com>
2022-04-19 12:26:57 +0700

    #29 - Add CustomOAuth2UserService

---
+++ src/main/java/com/hendisantika/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,26 @@
+package com.hendisantika.config.auth;
+
+import com.hendisantika.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mustache-post
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/04/22
+ * Time: 12.26
+ */
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+}
