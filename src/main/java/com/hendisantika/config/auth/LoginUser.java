eb703008730a65742feac9a697b2a62507eddb97
Hendi Santika <hendisantika@gmail.com>
2022-04-19 12:26:29 +0700

    #28 - Add LoginUser

---
+++ src/main/java/com/hendisantika/config/auth/LoginUser.java
@@ -0,0 +1,20 @@
+package com.hendisantika.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mustache-post
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/04/22
+ * Time: 12.26
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+}
