a5d08252582572f980da0ed4ae1be1703f07c271
Hendi Santika <hendisantika@gmail.com>
2022-04-19 11:55:28 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringBootMustachePostApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMustachePostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMustachePostApplication.class, args);
+    }
+
+}
