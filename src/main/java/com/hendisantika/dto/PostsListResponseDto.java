7a63a80180c5b982411e24344c7c0f1f2dbeff37
Hendi Santika <hendisantika@gmail.com>
2022-04-19 12:33:41 +0700

    #41 - Add PostsListResponseDto

---
+++ src/main/java/com/hendisantika/dto/PostsListResponseDto.java
@@ -0,0 +1,30 @@
+package com.hendisantika.dto;
+
+import com.hendisantika.domain.post.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mustache-post
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/04/22
+ * Time: 12.33
+ */
+@Getter
+public class PostsListResponseDto {
+    private final Long id;
+    private final String title;
+    private final String author;
+    private final LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
