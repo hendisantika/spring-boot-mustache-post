2d1e6d7daaf912bf96c35bec0cccf9f3374a28a3
Hendi Santika <hendisantika@gmail.com>
2022-04-19 12:38:52 +0700

    #51 - Add save in PostsApiController

--- src/main/java/com/hendisantika/controller/PostsApiController.java
+++ src/main/java/com/hendisantika/controller/PostsApiController.java
@@ -1,7 +1,10 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.dto.PostsSaveRequestDto;
 import com.hendisantika.service.PostsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,4 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostsApiController {
 
     private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
 }
