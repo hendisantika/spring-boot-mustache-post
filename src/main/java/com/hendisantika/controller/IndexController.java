5e672c4c8d8d8ff486944685218a45965436a046
Hendi Santika <hendisantika@gmail.com>
2022-04-19 12:37:33 +0700

    #47 - Add index in IndexController

--- src/main/java/com/hendisantika/controller/IndexController.java
+++ src/main/java/com/hendisantika/controller/IndexController.java
@@ -1,8 +1,12 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.config.auth.LoginUser;
+import com.hendisantika.dto.SessionUser;
 import com.hendisantika.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +22,13 @@ import org.springframework.stereotype.Controller;
 public class IndexController {
 
     private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "index";
+    }
 }
