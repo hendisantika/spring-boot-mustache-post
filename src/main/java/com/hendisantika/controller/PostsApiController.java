b918055f600b005758b2d177908d878e02bf7e4c
Hendi Santika <hendisantika@gmail.com>
2022-04-19 12:39:11 +0700

    #52 - Add update in PostsApiController

--- src/main/java/com/hendisantika/controller/PostsApiController.java
+++ src/main/java/com/hendisantika/controller/PostsApiController.java
@@ -1,9 +1,12 @@
 package com.hendisantika.controller;
 
 import com.hendisantika.dto.PostsSaveRequestDto;
+import com.hendisantika.dto.PostsUpdateRequestDto;
 import com.hendisantika.service.PostsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +28,11 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
     }
 
 }
